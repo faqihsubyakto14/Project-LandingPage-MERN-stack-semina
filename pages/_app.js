1e662a98bc4d390e01156834d8688e77774ea971
faqihsubyakto14 <faqihdev14@gmail.com>
2022-11-29 20:17:46 +0700

    First-Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import '../styles/main.css';
+import Head from 'next/head';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer position='top-right' />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
